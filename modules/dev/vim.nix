ed824901f0191d55b932e545801444836fd9f4b0
bear5 <john.xiongwu@gmail.com>
2020-09-04 22:17:28 -0500

    tab config for vim

--- modules/dev/vim.nix
+++ modules/dev/vim.nix
@@ -13,6 +13,14 @@
         set number relativenumber
         set incsearch
         set noswapfile
+        
+        set tabstop=2
+        set shiftwidth=2
+        set softtabstop=2
+        set expandtab
+        autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+
+        set backspace=2
      '';
      }
     )
