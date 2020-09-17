20148018b28a77c4c07a988845f5e80689eb73d4
bear5 <john.xiongwu@gmail.com>
2020-09-17 00:17:11 -0500

    auto-pair vim plugin

--- modules/dev/vim.nix
+++ modules/dev/vim.nix
@@ -8,6 +8,10 @@
 
      vim_configurable.customize {
      name = "vim";
+     vimrcConfig.packages.myPkg = with pkgs.vimPlugins; {
+      start = [ youcompleteme auto-pairs];
+     
+     };
      vimrcConfig.customRC = ''
         syntax enable
         set number relativenumber
@@ -21,7 +25,7 @@
         set expandtab
         autocmd FileType python setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab
         set autoindent
-        
+
         set backspace=2
         set wildmenu
         set wildmode=longest:full,full
