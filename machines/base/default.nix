5c75b15abda8b48a06a26f1a05505fc3e6dba910
bear5 <john.xiongwu@gmail.com>
2021-04-13 18:28:46 -0700

    pacific timezone

--- machines/base/default.nix
+++ machines/base/default.nix
@@ -32,7 +32,7 @@
   # +------------------------------------------------------------+
 
   # Set your time zone.
-  time.timeZone = "America/Chicago";
+  time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties
   i18n.defaultLocale = "en_US.UTF-8";
