58e7e6dbb80c1d013f170d448f86ed3df2caa4e8
bear5 <john.xiongwu@gmail.com>
2021-07-17 23:13:10 -0700

    add openvpn

--- machines/base/default.nix
+++ machines/base/default.nix
@@ -48,7 +48,7 @@
     rsync usbutils mkpasswd nixops p7zip unzip
     arandr smbclient neofetch ffmpeg zstd fd
     xclip okular fzf xorg.xmodmap xorg.xev xorg.xkbcomp
-    pciutils openconnect
+    pciutils openconnect openvpn
 
     # photo, video viewer and editor
     avidemux feh mplayer vlc
