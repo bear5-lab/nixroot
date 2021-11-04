ab7365a389882ba75863bcde3104eaa434b9fc8b
bear5 <john.xiongwu@gmail.com>
2021-11-04 08:39:53 -0700

    vscode remote development package

--- modules/dev/vscode.nix
+++ modules/dev/vscode.nix
@@ -16,6 +16,18 @@ let
       version = "11.1.3";
       sha256= "1x9bkf9mb56l84n36g3jmp3hyfbyi8vkm2d4wbabavgq6gg618l6";
     }
+    {
+      name="vscode-remote-extensionpack";
+      publisher = "ms-vscode-remote";
+      version = "0.21.0";
+      sha256 = "14l8h84kvnkbqwmw875qa6y25hhxvx1dsg0g07gdl6n8cv5kvy2g";
+    }
+    {
+      name="remote-ssh";
+      publisher = "ms-vscode-remote";
+      version = "0.66.0";
+      sha256 = "04jgn88b1j10qas4n6icg1zfvqqbxxmnjpvvkxdjgp845530k708";
+    }
   ]; 
 
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
