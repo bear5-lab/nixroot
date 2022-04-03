003034a78a8ab9552886f6b45bdb18d18815c84a
bear5 <john.xiongwu@gmail.com>
2022-04-03 06:55:25 -0700

    modify vscode

--- modules/dev/vscode.nix
+++ modules/dev/vscode.nix
@@ -17,17 +17,11 @@ let
       sha256= "1x9bkf9mb56l84n36g3jmp3hyfbyi8vkm2d4wbabavgq6gg618l6";
     }
     {
-      name="vscode-remote-extensionpack";
-      publisher = "ms-vscode-remote";
-      version = "0.21.0";
-      sha256 = "14l8h84kvnkbqwmw875qa6y25hhxvx1dsg0g07gdl6n8cv5kvy2g";
-    }
-    {
       name="remote-ssh";
       publisher = "ms-vscode-remote";
       version = "0.66.0";
       sha256 = "04jgn88b1j10qas4n6icg1zfvqqbxxmnjpvvkxdjgp845530k708";
-    }
+   }
   ]; 
 
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
