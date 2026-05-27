64cc4365d0b76b76aace1b4c05d76e05df505f77
bear5 <john.xiongwu@gmail.com>
2026-05-27 20:59:38 +0800

    add clash verge

--- machines/base/default.nix
+++ machines/base/default.nix
@@ -71,6 +71,9 @@ in {
     };
   };
 
+
+  # clash
+  programs.clash-verge.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash.completion.enable = true;
